936a7a9a3aa3677e5a279b2619ff55f736898e57
yecodeo <yecodeo@gmail.com>
2021-01-01 01:57:43 +0100

    Cleaning

--- frontapp/src/main.js
+++ frontapp/src/main.js
@@ -1,14 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import Buefy from 'buefy';
 
 import 'buefy/dist/buefy.css'
 
-
 Vue.config.productionTip = false
-Vue.use(Buefy, {
-  defaultIconPack: 'fab'
-});
 
 new Vue({
   render: h => h(App),
