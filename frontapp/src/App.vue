c337786c95a7c6938bd7f46283df43255cb94b0e
yecodeo <yecodeo@gmail.com>
2021-01-01 01:57:00 +0100

    Importing components

--- frontapp/src/App.vue
+++ frontapp/src/App.vue
@@ -1,36 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <article class="message">
-    <div class="message-header">
-      <p>Hello World</p>
-      <button class="delete" aria-label="delete"></button>
-    </div>
-    <div class="message-body">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-    </div>
-  </article>
+  <div class="container is-fluid">
+      <Header-app> </Header-app>
+        <section class="main-content columns is-fullheight">
+          <Menu-app></Menu-app>
+          <Core-app></Core-app>
+        </section>
   </div>
+
 </template>
 
 <script>
-import 'bulma';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import CoreUi from './components/coreUi';
+
 
 export default {
   name: 'App',
   components: {
-    
+    'Header-app': Header,
+    'Menu-app': Menu,
+    'Core-app': CoreUi,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
