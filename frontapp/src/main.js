c7db8ed96b542f24021b6e0a14cadd42a455f61b
yecodeo <yecodeo@gmail.com>
2021-01-01 01:57:37 +0100

    Importing buefy

--- frontapp/src/main.js
+++ frontapp/src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Buefy from 'buefy';
+
+import 'buefy/dist/buefy.css'
+
 
 Vue.config.productionTip = false
+Vue.use(Buefy, {
+  defaultIconPack: 'fab'
+});
 
 new Vue({
   render: h => h(App),
