1e84890330984502c68cc0398439157cec114532
yecodeo <yecodeo@gmail.com>
2021-01-02 05:37:45 +0100

    create publication

---
+++ frontapp/src/components/user/publications.vue
@@ -0,0 +1,117 @@
+<template>
+	<section>
+		<div class="form">
+			<div class="columns">
+				<div class="column is-two-fifths">
+					<b-field label="Prénom">
+						<b-input></b-input>
+					</b-field>
+					<b-field label="Nom">
+						<b-input></b-input>
+					</b-field>
+					<b-field label="About me">
+						<b-input type="textarea" minlength="10" maxlength="100"></b-input>
+					</b-field>
+					<h6 class="title is-6">Social Ids</h6>
+					
+					<div class="media-social">
+						<div v-for="(media, index) in socialInputs" :key="`social-${index}`" >
+							<b-field>
+								<b-input v-model="media.value" expanded></b-input>
+								<b-select v-model="media.select" placeholder="selectionnez un media">
+									<option v-for="(option, key) in socials" :value="option" :key="key">
+										{{ option }}
+									</option>
+								</b-select>
+								<b-button @click="addField" type="is-success is-light">+</b-button>
+								<b-button @click="removeField" :disabled="socialInputs.length <= 1" type="is-danger is-light">-</b-button>
+							</b-field>
+						</div>
+					</div>
+
+					<b-button class="my-4" v-on:click="toggle" type="is-primary is-light">Ajouté une affiliation</b-button>
+
+					<template v-if="showAffeliation">
+						<b-field label="Organisation">
+							<b-select placeholder="Selectionnez une organisation" expanded>
+								<option v-for="(org, key) in organisations" :value="org" :key="key">
+									{{ org }}
+								</option>
+							</b-select>
+						</b-field>
+						<b-field label="Équipe">
+							<b-input></b-input>
+						</b-field>
+						<template>
+							<h6 class="title is-6">Dates</h6>
+							<div class="columns">
+								<div class="column is-6">
+									<b-field>
+										<b-datepicker placeholder="cliquez pour choisir" v-model="dates.starts">
+										</b-datepicker>
+									</b-field>
+								</div>
+								<div class="column is-6">
+									<b-field>
+										<b-datepicker placeholder="cliquez pour choisir" v-model="dates.ends">
+										</b-datepicker>
+									</b-field>
+								</div>
+
+							</div>
+							<b-field label="Pays">
+								<b-select placeholder="Selectionnez un pays" expanded>
+									<option v-for="(el, key) in pays" :value="el" :key="key">
+										{{ el }}
+									</option>
+								</b-select>
+							</b-field>
+						</template>
+					</template>
+
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+<script>
+	export default {
+		name: 'Publication',
+		data() {
+			return {
+				dates: {
+					starts: null,
+					ends: null
+				},
+				showAffeliation: true,
+				socialInputs: [ { value: '', select: 'LinkedIn'} ],
+				socials: ['Facebook', 'Twitter', 'LinkedIn', 'Orcid'],
+				pays: ['France', 'Belgique', 'Espagne', 'Italie'],
+				organisations: ['Énergie, Recherche et Science', 'Économie et Société numériques',
+					'Protection des consommateurs', 'Budget / Santé	'
+				],
+			}
+		},
+		methods: {
+			toggle: function() {
+				this.showAffeliation = !this.showAffeliation;
+			},
+			addField: function() {
+				const id = this.socialInputs.length + 1;
+				this.socialInputs.push( { value: '', select: ''} );
+				console.log(this.socialInputs[0].value);
+			},
+			removeField: function(el) {
+				const index = this.socialInputs.indexOf(el);
+				this.socialInputs.splice(index, 1);
+				console.log(this.socialInputs.length);
+			}
+
+		}
+
+	}
+</script>
+
+<style>
+
+</style>
