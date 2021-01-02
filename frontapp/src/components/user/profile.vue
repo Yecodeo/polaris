8f2ef2ef60c9006e8e34d9c0f3122f58323deafa
yecodeo <yecodeo@gmail.com>
2021-01-02 03:02:49 +0100

    Create prfil page

---
+++ frontapp/src/components/user/profile.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<div class="card mb-4">
+			<div class="card-content">
+				<div class="media">
+					<div class="media-content">
+						<p class="title is-5">Comissariate à l'énergie atomique</p>
+						<p class="subtitle is-6">Cherchereur poste doctoral</p>
+					</div>
+				</div>
+
+				<div class="content">
+					<p class="subtitle is-5 mb-1">Équipe de recherche en fission nucléaire</p>
+					<p class="subtitle is-6 mb-1">depuis 2017</p>
+					<span>France</span>
+				</div>
+			</div>
+		</div>
+		<div class="card mb-4">
+			<div class="card-content">
+				<div class="media">
+					<div class="media-content">
+						<p class="title is-5">Comissariate à l'énergie atomique</p>
+						<p class="subtitle is-6">Cherchereur poste doctoral</p>
+					</div>
+				</div>
+
+				<div class="content">
+					<p class="subtitle is-5 mb-1">Équipe de recherche en fission nucléaire</p>
+					<p class="subtitle is-6 mb-1">depuis 2017</p>
+					<span>France</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Profile'
+	}
+</script>
+
+<style>
+	.card {
+		box-shadow: 0 0.5em 1em -0.425em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02) !important;
+	}
+
+	.b-rounded {
+		border-radius: 0.5rem 0.5rem 0rem 0rem !important;
+	}
+
+	.boxed {
+		border: 1px solid #c1c1c1;
+	}
+</style>
