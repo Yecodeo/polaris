d8dff9ece92655947c2888b656ef4dce53c84094
yecodeo <yecodeo@gmail.com>
2021-01-02 03:02:16 +0100

    Create Layout folder

---
+++ frontapp/src/components/layout/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="columns is-mobile is-centered mt-1 mb-0">
+      <div class="column is-half">
+        <b-field>
+          <b-input placeholder="Chercher un utilisateur" expanded>
+            <option>
+                <!-- v-for="option in data" -->
+                <!-- :value="option.id" -->
+                <!-- :key="option.id"> -->
+                <!-- {{ option.user.first_name }} -->
+              </option>
+          </b-input>
+        </b-field>  
+      </div>
+    </div>
+    <div class="columns is-mobile is-centered mt-1">
+      <div class="column is-half has-text-centered pt-0">
+          <b-button type="is-primary">recherche</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header-app',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ frontapp/src/components/layout/coreUi.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container column is-10">
+    <div class="section pt-6">
+      <section class="boxed px-2">
+        <div class="header-content">
+          <b-tabs position="is-centered is-boxed" class="block mt-5">
+            <b-tab-item headerClass="b-rounded has-background-light" label="Vue générale">
+            </b-tab-item>
+            <b-tab-item headerClass="b-rounded has-background-light" label="My profil">
+                <Profile />
+            </b-tab-item>
+            <b-tab-item headerClass="b-rounded has-background-light" label="Mes publications">
+              <Publication />
+            </b-tab-item>
+          </b-tabs>
+        </div>
+        <div class="content">
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Profile from '../user/profile';
+  import Publication from '../user/publications';
+  export default {
+    name: 'core-ui',
+    components: {
+      Profile,
+      Publication
+    }
+  }
+</script>
+<style></style>
