0dc2f56b2fc0eaf9b253ab7390449d276ac4c7f7
yecodeo <yecodeo@gmail.com>
2021-01-02 06:23:43 +0100

    create general page

---
+++ frontapp/src/components/user/General.vue
@@ -0,0 +1,31 @@
+<template>
+	<section>
+		<div class="columns is-mobile is-centered mt-1 mb-0">
+			<div class="column is-half">
+				<div class="columns">
+					<div class="column is-8">
+						<b-field expanded>
+							<b-input placeholder="Rechercher dans les publications"></b-input>
+						</b-field>
+					</div>
+					<div class="column is-4">
+						<div class="buttons">
+							<b-button type="is-primary" expanded>Recherche</b-button>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: 'General'
+	}
+</script>
+
+<style>
+
+</style>
