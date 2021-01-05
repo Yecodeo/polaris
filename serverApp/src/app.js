10d5e34a3b00e2a835148a991313b834d58c97c6
yecodeo <yecodeo@gmail.com>
2021-01-06 00:45:45 +0100

    refactoring

--- serverApp/src/app.js
+++ serverApp/src/app.js
@@ -4,7 +4,8 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import bodyparser from 'body-parser';
 
-import indexRouter from './routes/index';
+import publicationRouter from './routes/punblication.route';
+import userRouter from './routes/user.route';
 
 let app = express();
 app.use(bodyparser.json())
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', publicationRouter);
+app.use('/', userRouter);
 
 export default app;
