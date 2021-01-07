a4e593ec35041efcb5cb8190661d2f541cbfaefe
yecodeo <yecodeo@gmail.com>
2021-01-07 01:57:44 +0100

    add route country

--- serverApp/src/app.js
+++ serverApp/src/app.js
@@ -3,9 +3,13 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import bodyparser from 'body-parser';
+import elastic from './db/elastic';
+
+const client = elastic.getInstance();
 
 import publicationRouter from './routes/punblication.route';
 import userRouter from './routes/user.route';
+import countryRoute from './routes/country.router';
 
 let app = express();
 app.use(bodyparser.json())
@@ -17,5 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', publicationRouter);
 app.use('/', userRouter);
+app.use('/', countryRoute);
+
+app.use((req, res, next) => {
+	esLog(client, req);
+	next();
+  })
 
 export default app;
