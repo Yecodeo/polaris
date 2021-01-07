ce7b1aa5633b39682f8938185341b88c11ba0fc4
yecodeo <yecodeo@gmail.com>
2021-01-07 01:56:11 +0100

    create seeds for es

--- serverApp/src/db/seeds/seed.eleasticsearch.js
+++ serverApp/src/db/seeds/seed.eleasticsearch.js
@@ -1,48 +1,49 @@
-import elastic from '../elastic';
-const client = elastic.getInstance();
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({
+	node: `http://elasticsearch:9200`
+});
 
 const seeds = [
-	// {
-	// 	name: 'country',
-	// 	json: require('./data/countries.json'),
-	// },
+	{
+		name: 'country',
+		json: require('./data/countries.json'),
+	},
 	{
 		name: 'publication',
 		json: require('./data/publication.json')
 	},	
 ]
-
 seeds.map((seed) => {
-	pushBulk(seed.json, seed.name);
+	client.indices.exists({index: seed.name}, (err, res, status) => {
+		if (res.statusCode === 404 || null) {
+			pushBulk(seed.json, seed.name);
+		}
+	})
 })
+
 async function pushBulk(data, index) {	
-	  // prepare array for bulk
-	  const body = data.flatMap(doc => [{ index: { _index: index } }, doc])
-	  
-	  const { body: bulkResponse } = await client.bulk({ refresh: true, body })
+	// prepare array for bulk
+	const body = data.flatMap(doc => [{ index: { _index: index } }, doc])
 
-  if (bulkResponse.errors) {
-    const erroredDocuments = []
-    // The items array has the same order of the dataset we just indexed.
-    // The presence of the `error` key indicates that the operation
-    // that we did for the document has failed.
-    bulkResponse.items.forEach((action, i) => {
-      const operation = Object.keys(action)[0]
-      if (action[operation].error) {
-        erroredDocuments.push({
-          // If the status is 429 it means that you can retry the document,
-          // otherwise it's very likely a mapping error, and you should
-          // fix the document before to try it again.
-          status: action[operation].status,
-          error: action[operation].error,
-          operation: body[i * 2],
-          document: body[i * 2 + 1]
-        })
-      }
-    })
-    console.log(erroredDocuments)
+	const { body: bulkResponse } = await client.bulk({ refresh: true, body })
 
-	  const { body: count } = await client.count({ index: index })
-	  console.log(`${count.count} added`)
-}
+	if (bulkResponse.errors) {
+		const erroredDocuments = []
+		bulkResponse.items.forEach((action, i) => {
+		const operation = Object.keys(action)[0]
+		if (action[operation].error) {
+			erroredDocuments.push({
+
+			status: action[operation].status,
+			error: action[operation].error,
+			operation: body[i * 2],
+			document: body[i * 2 + 1]
+			})
+		}
+		})
+		console.log(erroredDocuments)
+
+		const { body: count } = await client.count({ index: index })
+		console.log(`${count.count} added`)
+	}
 }
