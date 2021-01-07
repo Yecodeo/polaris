e1a99be04adeded29db6328c9c6df2989b6ff424
yecodeo <yecodeo@gmail.com>
2021-01-07 01:54:29 +0100

    refactoring creation index

--- serverApp/src/db/indicies/publication.js
+++ serverApp/src/db/indicies/publication.js
@@ -3,47 +3,28 @@ const indexName = path.basename(__filename).split('.')[0];
 
 
 /**
- * Create index
- * @param {*} client 
+ * set mapping of created index
+ * @param {} client 
  */
 export default function (client) {
-	if (indexName) {
-		client.indices.exists({index: indexName}, (err, res, status) => {
-			if (res) {
-				console.info(`L'index ${indexName} existe déjà`);
-			} else {
-				client.indices.create( {index: indexName}, (err, res, status) => {
-				setMapping(client)
-				console.info(err, res, status);
-			})
-		  }
-		})
-	}
-
-	/**
-	 * set mapping of created index
-	 * @param {} client 
-	 */
-	function setMapping (client) {
-		console.info(`creating ${indexName} mapping...`);
-		client.indices.putMapping({
-			index: indexName,
-			body: {
-			properties: { 
-				title: { type: 'text' },
-				owner: { type: 'text' },
-				auteurs: { type: 'text' },
-				annee: { type: 'date' }, 
-				lang: { type: 'text' }
+	console.info(`creating ${indexName} mapping...`);
+	client.indices.putMapping({
+		index: indexName,
+		body: {
+			properties: {
+				title: {type: 'text'},
+				owner: {type: 'text'},
+				auteurs: {type: 'text'},
+				annee: {type: 'date'},
+				lang: {	type: 'text'}
 			}
 		}
-		}, (err,resp, status) => {
-			if (err) {
-			  console.error(err, status);
-			}
-			else {
-				console.log(`${indexName} Successfully Created`, status, resp);
-			}
-		});
-	}
+	}, (err, resp, status) => {
+		if (err) {
+			console.error(err, status);
+		} else {
+			console.log(`${indexName} Successfully Created`, status, resp);
+		}
+	});
+
 }
