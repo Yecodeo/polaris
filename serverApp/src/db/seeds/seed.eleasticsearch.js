a53b6a4add4158db668837368560e695754cdc17
yecodeo <yecodeo@gmail.com>
2021-01-11 03:35:54 +0100

    Formatting code

--- serverApp/src/db/seeds/seed.eleasticsearch.js
+++ serverApp/src/db/seeds/seed.eleasticsearch.js
@@ -1,35 +1,35 @@
 import axios from 'axios';
-const uri = `http://localhost:9200`;
+
+const uri = 'http://localhost:9200';
 
 const seeds = [{
-		name: 'country',
-		json: require('./data/countries.json'),
-	},
-	{
-		name: 'user',
-		json: require('./data/users.json')
-	},
-]
+  name: 'country',
+  json: require('./data/countries.json'),
+},
+{
+  name: 'user',
+  json: require('./data/users.json'),
+},
+];
 
 seeds.map(async (seed) => {
-	// delete documents
-	axios.post(`${uri}/${seed.name}/_delete_by_query`, {
-		"query": {
-			"match_all": {}
-		}
-	}).then(res => {
-		console.log(res)
-	}).catch(error => {
-		console.error(error)
-	});
+  // delete documents
+  axios.post(`${uri}/${seed.name}/_delete_by_query`, {
+    query: {
+      match_all: {},
+    },
+  }).then((res) => {
+    console.log(res);
+  }).catch((error) => {
+    console.error(error);
+  });
 
-	// seed data base
-	seed.json.forEach(el => {
-		axios.post(`${uri}/${seed.name}/_doc`, el).then(response => {
-			console.log(response)
-		}).catch(error => {
-			console.error(error)
-		});
-	})
-
+  // seed data base
+  seed.json.forEach((el) => {
+    axios.post(`${uri}/${seed.name}/_doc`, el).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.error(error);
+    });
+  });
 });
