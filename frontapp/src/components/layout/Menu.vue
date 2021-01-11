724e7596319ebe8d8736157675fdd92943c61197
yecodeo <yecodeo@gmail.com>
2021-01-11 03:31:20 +0100

    Consuming data from store

--- frontapp/src/components/layout/Menu.vue
+++ frontapp/src/components/layout/Menu.vue
@@ -3,22 +3,24 @@
     <ul class="menu-list">
       <li>
         <b-image
-            src="https://picsum.photos/600/400"
+            :src="$store.getters.getProfil.avatar"
             alt="A random image"
             ratio="6by4"
         />
       </li>
       <li>
         <div class="identity mt-1">
-          <span class="is-capitalized has-text-weight-semibold">prénom </span>
-          <span class="is-capitalized has-text-weight-semibold">nom</span>  
+          <span class="is-capitalized has-text-weight-semibold">{{ $store.getters.getUser.prefix}} </span>
+          <span class="is-capitalized has-text-weight-semibold">{{ $store.getters.getUser.firstname}} </span>
+          <span class="is-capitalized has-text-weight-semibold">{{ $store.getters.getUser.lastname }}</span>  
         </div>
       </li>
       <li>
         <div class="aboutme mt-3">
           <h6 class="title is-6">About me</h6>
-          <p class="subtitle is-6 has-text-justified">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Nam consequuntur qui eligendi aspernatur ullam doloremque non minima, quae ex voluptas ?</p>
+          <p class="subtitle is-6 has-text-justified">
+            {{ $store.getters.getProfil.aboutme }}
+          </p>
         </div>
 
       </li>
@@ -28,26 +30,11 @@
         </h6>
         <div class="level">
           <div class="level-left">
-            <div class="level-item m-0">
-              <a href="#" class="p-0 m-0">
-                <Facebook />
+            <template v-for="(social, key) in $store.getters.getProfil.socials">
+              <a :key="key" :href="social.link" class="p-0 m-0">
+                <component :is="social.name"  ></component>
               </a>
-            </div>
-            <div class="level-item m-0">
-              <a href="#" class="p-0 m-0">
-                <Linkedin />
-              </a>
-            </div>
-            <div class="level-item m-0">
-              <a href="#" class="p-0 m-0">
-                <Twitter />
-              </a>
-            </div>
-            <div class="level-item m-0">
-              <a href="#" class="p-0 m-0">
-                <Orcid />
-              </a>
-            </div>
+            </template>
           </div>
         </div>
       </li>
