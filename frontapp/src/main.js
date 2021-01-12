37d5d4ccf6b57a86530fd32fb249620616dc6116
yecodeo <yecodeo@gmail.com>
2021-01-12 02:49:48 +0100

    import capitalize filter

--- frontapp/src/main.js
+++ frontapp/src/main.js
@@ -5,11 +5,13 @@ import App from './App.vue';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css'
 import Route from './routes/route';
-import store from './store'
+import store from './store';
+import capitalize from './helper/capitalize';
 
 Vue.config.productionTip = false
 Vue.use(Buefy);
 Vue.use(VueRouter);
+Vue.filter('capitalize', capitalize);
 
 const router = new VueRouter({
   linkActiveClass: 'is-active',
