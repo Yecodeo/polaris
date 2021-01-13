5c08d6c85496d2a96c2849f43842ebe63a8aa864
yecodeo <yecodeo@gmail.com>
2021-01-13 08:27:32 +0100

    clean un used code

--- frontapp/src/store/index.js
+++ frontapp/src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios';
 
 Vue.use(Vuex)
 
@@ -13,38 +12,17 @@ export default new Vuex.Store({
   },
   getters: {
     getApiUrl: state => state.api_url,
-    getUser: state => state.user,
-    getProfil: state => state.user.profil,
-    getAffiliation: state => state.affiliation,
-    getCountry: state => state.user.country
+    getUser: state => state.user
   },
   mutations: {
     setUser(state, payload) {
       state.user = payload;
-    },
-    setCountry(state, payload) {
-      state.profil.country = payload;
-    },
-    setAffiliation(state, payload) {
-      state.affiliation = payload;
     }
   },
   actions: {
     setUser(state, payload) {
       state.commit('setUser', payload);
-    },
-    setCountry(state, payload){
-      state.commit('setCountry', payload);
-    },
-    async setAffiliation(state) {
-      const id = state.state.user.id;
-      const url = state.state.api_url;
-
-      await axios.get(`${url}/affiliation/${id}`).then(function(res) {
-        state.commit('setAffiliation', res.data.data);
-      }).catch(error => console.error(error));
     }
-
   },
   modules: {
   }
