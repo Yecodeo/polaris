1548ebb78b0ee082eb1fa9418320a1241aa85283
yecodeo <yecodeo@gmail.com>
2021-01-13 19:56:43 +0100

    refresh pages when selecting a result

--- frontapp/src/App.vue
+++ frontapp/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container is-fluid">
       <Header-app> </Header-app>
-        <section v-if="$store.getters.getUser.id" class="main-content columns is-fullheight">
+        <section v-if="$store.getters.getUser" :key="$store.getters.getUser.id" class="main-content columns is-fullheight">
           <Menu-app></Menu-app>
           <Core-app></Core-app>
         </section>
@@ -21,7 +21,13 @@ export default {
     'Header-app': Header,
     'Menu-app': Menu,
     'Core-app': Body,
+  },
+  mounted(){
+      this.$store.watch(this.$store.getters.getUser, () => {
+        console.log('changed')
+      })
   }
+
 }
 </script>
 
