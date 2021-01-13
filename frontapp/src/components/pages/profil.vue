d7f3c382e0e5f4dff230ce2991086abe7dc16ada
yecodeo <yecodeo@gmail.com>
2021-01-13 04:15:08 +0100

    externalize add affiliation

--- frontapp/src/components/pages/profil.vue
+++ frontapp/src/components/pages/profil.vue
@@ -37,44 +37,7 @@
 						class="delete my-4 is-pulled-right has-background-danger">
 					</a>
 					<template v-if="showAffeliation">
-						
-						<b-field label="Organisation">
-							<b-select required placeholder="Selectionnez une organisation" expanded>
-								<option v-for="(org, key) in organisations" :value="org" :key="key">
-									{{ org }}
-								</option>
-							</b-select>
-						</b-field>
-						<b-field label="Équipe">
-							<b-input></b-input>
-						</b-field>
-						<h6 class="title is-6">Dates</h6>
-						<div class="columns">
-							<div class="column is-6">
-								<b-field>
-									<b-datepicker placeholder="cliquez pour choisir" v-model="dates.starts">
-									</b-datepicker>
-								</b-field>
-							</div>
-							<div class="column is-6">
-								<b-field>
-									<b-datepicker placeholder="cliquez pour choisir" v-model="dates.ends">
-									</b-datepicker>
-								</b-field>
-							</div>
-						</div>
-						<div class="columns">
-							<div class="column is-full">
-								<Autocomplete 
-									:api="api_url" 
-									:values="['name']"
-									label="Pays"
-									dispatch="setCountry"
-									required
-								/>
-							</div>
-						</div>
-
+						<AddAffiliation />
 					</template>
 
 				</div>
@@ -90,16 +53,16 @@
 
 <script>
 	import Upload from '../common/Upload';
-	import Autocomplete from '../common/Autocomplete';
 	import Social from '../common/Social';
-	import save from '../../helper/save';
+	import {update} from '../../helper/save';
+	import AddAffiliation from '../common/addAffiliation'
 
 	export default {
 		name: 'Profil',
 		components: {
 			Upload,
-			Autocomplete,
 			Social,
+			AddAffiliation
 		},
 		data() {
 			return {
@@ -111,25 +74,29 @@
 					ends: null
 				},
 				showAffeliation: true,
-				country: {},
+				affiliation: {
+					organisation: '',
+					equipe: '',
+					date: {
+						lte: '',
+						gte: ''
+					},
+					pays: ''
+				},
 				organisations: ['Énergie, Recherche et Science', 'Économie et Société numériques',
 					'Protection des consommateurs', 'Budget / Santé	'
 				],
-				name: ''
-
 			}
 		},
 		beforeMount() {
 			this.user = this.$store.getters.getUser;
 		},
 		mounted() {
-			this.api_url = `${this.$store.getters.getApiUrl}`,
-			this.updateUser = `${this.api_url}/user/${this.user.id}`
+			this.updateUser = `${this.$store.getters.getApiUrl}/user/${this.user.id}`
 		},
 		methods: {
 			persiste: function(body) {
-				const url = `${this.api_url}/user/${this.user.id}`
-				save(url, body);
+				update(this.updateUser, body);
 			},
 			toggle: function () {
 				this.showAffeliation = !this.showAffeliation;
